e25c874717af1c59aadc9b1760ec6066357db947
teye <teyezilliqa.com>
2022-05-27 14:15:55 +0800

    improve styles

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -10,18 +10,18 @@ function App() {
       <div>
         <div className="container mx-auto">
           <div className="flex flex-col mt-8 items-center">
-            <Link to={"/project1"}>
+            <Link to={"/project1"} className="main-div-hover">
               <div className="bg-white rounded-xl shadow-lg h-24 lg:min-w-[60rem] md:min-w-[40rem] p-4 my-4 border border-slate-300">
-                <h2 className="font-semibold text-base text-slate-900">Project 1</h2>
+                <h2 className="font-medium text-base text-slate-900">Project 1: Simple Counter</h2>
                 <p className="text-sm text-gray-700 mt-2">Simple counter example to learn how to call ethereum contracts and display them on frontend.</p>
               </div>
             </Link>
             <div className="bg-white rounded-xl shadow-lg h-24 lg:min-w-[60rem] md:min-w-[40rem] p-4 my-4 border border-slate-300">
-              <h2 className="font-semibold text-base text-slate-900">Project 2</h2>
+              <h2 className="font-medium text-base text-slate-900">Project 2: NFT</h2>
               <p className="text-sm text-gray-700 mt-2">ERC-721 example to learn about ethereum NFT</p>
             </div>
             <div className="bg-white rounded-xl shadow-lg h-24 lg:min-w-[60rem] md:min-w-[40rem] p-4 my-4 border border-slate-300">
-              <h2 className="font-semibold text-base text-slate-900">Project 3</h2>
+              <h2 className="font-medium text-base text-slate-900">Project 3: Marketplace</h2>
               <p className="text-sm text-gray-700 mt-2">NFT marketplace example to learn about contract to contract interactions.</p>
             </div>
           </div>
--- dapp/src/components/simpleCounterExample.js
+++ dapp/src/components/simpleCounterExample.js
@@ -93,41 +93,64 @@ function SimpleCounterExample() {
         <div className="max-w-full">
             <Navbar/>
             <div className="container mx-auto">
-                <h1 className="font-semibold text-3xl my-4">Project 1</h1>
-                <h2 className="font-semibold text-lg text-blue-600">Instructions</h2>
-                <button 
-                    className="bg-teal-500 font-semibold text-white py-2 px-4 border border-teal-500 hover:border-transparent rounded"
-                    onClick={onConnectWallet}>
-                    Connect to Metamask
-                </button>
+                <h1 className="font-semibold text-3xl my-4">Counter Example</h1>
+                <h2 className="font-semibold text-lg text-blue-800 mb-2">Instructions</h2>
+                <p>This is a simple contract that does incrementing and reseting of numbers.</p>
+                <p>Users connect with ther Metamask to retrieve their account information and interact with the contract.</p>
+                <ol className="list-decimal mt-4 ml-4">
+                  <li>Click on "Connect to Metamask" and view your account information.</li>
+                  <li>Click on "Get Number" to fetch the current number from the contract.</li>
+                  <li>Click on "Increment" to add "1" to the current number.</li>
+                  <li>Click on "Reset" to reset the number to 1.</li>
+                  <li>If you change account on Metamask, click on "Connect to Metamask" to reload.</li>
+                </ol>
 
-                {
-                    currentAccount &&
-                    <div>
-                        <p>Account: {currentAccount}</p>
-                        <p>Balance: {balance} ETH</p>
-                        <p>Chain ID: {chainId}</p>
-                        <p>Chain: {chainName}</p>
+                <div className="mt-8 mb-8">
+                    <h3 className="font-bold text-[1rem] text-blue-800 uppercase mb-2">Account Info</h3>
+                    <div className="mb-4">
+                      <button 
+                          className="bg-teal-500 font-semibold text-white text-sm py-2 px-4 border border-teal-500 hover:border-transparent rounded"
+                          onClick={onConnectWallet}>
+                          Connect to Metamask
+                      </button>
                     </div>
-                }
-                <p><strong>Simple Counter Contract</strong>: {Contracts.COUNTER_CONTRACT}</p>
-                <p><strong>Number</strong>: {number}</p>
+                    {
+                      currentAccount ?
+                      <div>
+                        <p><strong>Account</strong>: {currentAccount}</p>
+                        <p><strong>Balance</strong>: {balance} ETH</p>
+                        <p><strong>Chain ID</strong>: {chainId}</p>
+                        <p><strong>Chain</strong>: {chainName}</p>
+                      </div>
+                      :
+                      <span className="text-gray-600">No account found. Click on "Connect to Metamask."</span>
+                    }
+                </div>
 
-                <button 
-                    className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4"
-                    onClick={() => onIncrement()}>
-                    Increment
-                </button>
-                <button 
-                    className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4"
-                    onClick={() => onGetNumber()}>
-                    Get Number
-                </button>
-                <button 
-                    className="bg-red-500 text-white font-semibold py-2 px-4 border border-red-500 rounded"
-                    onClick={() => onResetNumber()}>
-                    Reset
-                </button>
+                <div>
+                  <h3 className="font-bold text-[1rem] text-blue-800 uppercase mb-2">Simple Counter Contract</h3>
+                  <p>Click on "Connect to Metamask" to connect your wallet before clicking on any of these buttons.</p>
+                  <p className="mt-4"><strong>Contract Address</strong>: {Contracts.COUNTER_CONTRACT}</p>
+                  <p><strong>Number</strong>: {number}</p>
+                  <div className="mt-4">
+                    {/* contract buttons */}
+                    <button 
+                        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4"
+                        onClick={() => onIncrement()}>
+                        Increment
+                    </button>
+                    <button 
+                        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4"
+                        onClick={() => onGetNumber()}>
+                        Get Number
+                    </button>
+                    <button 
+                        className="bg-red-500 text-white font-semibold py-2 px-4 border border-red-500 rounded"
+                        onClick={() => onResetNumber()}>
+                        Reset
+                    </button>
+                  </div>
+                </div>
             </div>
         </div>
     );

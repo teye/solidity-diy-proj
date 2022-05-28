26b6f45c77e40838ec5f571a3de59056820f8e4f
teye <teyezilliqa.com>
2022-05-28 14:48:38 +0800

    refactor fish example

--- contracts/contracts/FishNFT.sol
+++ contracts/contracts/FishNFT.sol
@@ -5,6 +5,12 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
+/*
+ * Custom errors
+ */
+error PriceNotMet();
+
 /*
  * Self-learning ERC721 contract
  * Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
@@ -15,6 +21,7 @@ contract FishNFT is ERC721, ERC721Enumerable {
     Counters.Counter private _tokenIds;
 
     // events
+    event MintFish(address indexed _from, string _species, uint256 _tokenId);
     event LevelUp(address indexed _from, uint256 _tokenId, uint256 _level);
 
     // fish details
@@ -55,26 +62,36 @@ contract FishNFT is ERC721, ERC721Enumerable {
     }
 
     /*
-    * @notice mints a Fish to the sender; anyone can mint
+    * @notice mints a Fish to the sender; anyone can mint, each fish cost 0.001 eth
     * @param  string _species from UI - "GUPPY", "GOLDFISH", "SHARK"
     * @return minted token ID
     */ 
-    function mintFish(string memory _species) public returns (uint256) {
+    function mintFish(string memory _species) external payable returns (uint256) {
+        if (msg.value < 1000000000000000 wei) {
+            revert PriceNotMet();
+        }
+
         _tokenIds.increment();
 
         uint256 newFishId = _tokenIds.current();
         _safeMint(msg.sender, newFishId);
         fishes.push(Fish(_species, newFishId, 1));
 
+        emit MintFish(msg.sender, _species, newFishId);
+
         return newFishId;
     }
 
     /*
-    * @notice level up a Fish; only token owner can level up their own fish
+    * @notice level up a Fish; only token owner can level up their own fish; cost 0.002 eth per level
     * @param  uint256 tokenId   NFT token id to level up
     * @dev    SafeMath not required for Solidity 0.8; See https://docs.openzeppelin.com/contracts/4.x/api/utils#SafeMath
     */
-    function levelUp(uint256 tokenId) external onlyTokenOwner(tokenId) {
+    function levelUp(uint256 tokenId) external payable onlyTokenOwner(tokenId) {
+        if (msg.value < 2000000000000000 wei) {
+            revert PriceNotMet();
+        }
+
         // subtract 1 because array is 0-based but token index is 1
         uint256 arrayIdx = tokenId - 1;
         require(fishes[arrayIdx].tokenId == tokenId);
--- dapp/src/components/nftExample.js
+++ dapp/src/components/nftExample.js
@@ -1,11 +1,106 @@
 import Navbar from "./navbar";
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import { Contracts } from "../utils/utils";
+import { FISHNFTABI } from "../abi/fishNFTABI";
 
+
+let provider;
+
+/**
+ * ERC-721 example
+ * Users can mint a Fish NFT and see the list of fishes that he/she owns
+ * 
+ */
 function NFTExample() {
+    const [currentAccount, setCurrentAccount] = useState("");
+
+    const onConnectWallet = async () => {
+        if (!window.ethereum) {
+          console.log("Please install Metamask");
+          return;
+        }
+    
+        provider = new ethers.providers.Web3Provider(window.ethereum);
+        
+        // MetaMask requires requesting permission to connect users accounts
+        try {
+            const accounts = await provider.send("eth_requestAccounts", []);
+            setCurrentAccount(accounts[0]);
+        } catch(e) {
+            console.error(e);
+        }
+    }
+
+    /**
+     * call mintFish()
+     * each minting costs 0.001 eth
+     */
+    const onMint = async () => {
+        await onConnectWallet();
+        const signer = provider.getSigner();
+        const deployed = new ethers.Contract(Contracts.FISH_NFT_CONTRACT, FISHNFTABI, signer);
+        console.log("deployed: ", deployed);
+
+        try {
+            const tx = await deployed.mintFish("happy", {
+                value: ethers.utils.parseEther("0.001")
+            });
+            console.log("tx: ", tx.hash);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    /**
+     * call levelUp()
+     * each levelling costs 0.002 eth
+     * require tokenId
+     */
+    const onLevelUp = async () => {
+        await onConnectWallet();
+        const signer = provider.getSigner();
+        const deployed = new ethers.Contract(Contracts.FISH_NFT_CONTRACT, FISHNFTABI, signer);
+        console.log("deployed: ", deployed);
+
+        try {
+            const tx = await deployed.levelUp(1, {
+                value: ethers.utils.parseEther("0.002")
+            });
+    
+            console.log("tx: ", tx.hash);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
     return (
         <div className="max-w-full">
             <Navbar />
             <div className="container mx-auto">
-                <h1 className="font-semibold text-3xl my-4">ERC-721 Example</h1>
+                <h1 className="font-semibold text-3xl my-4">"What The Fish": ERC-721 Example</h1>
+                <button 
+                    className="bg-blue-500 font-semibold text-white text-sm py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+                    onClick={onConnectWallet}>
+                    Connect to Metamask
+                </button>
+                {
+                    currentAccount &&
+                    <div>Wallet: {currentAccount}</div>
+                }
+                {/* contract buttons */}
+                <div className="mt-4">
+                    <button 
+                        className="bg-transparent hover:bg-blue-500 text-blue-700 text-sm font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4"
+                        onClick={() => onMint()}>
+                        Mint a Fish
+                    </button>
+                    <button 
+                        className="bg-transparent hover:bg-blue-500 text-blue-700 text-sm font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded mr-4"
+                        onClick={() => onLevelUp()}>
+                        Level up a Fish
+                    </button>
+                </div>
             </div>
         </div>
     );
